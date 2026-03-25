b49061b617a3f4f94f855b29ede3ebb82fc90de2
haridtvt <tranvanhaidtvt@gmail.com>
2026-03-25 15:34:49 +0000

    Update scanSonar.groovy

--- vars/scanSonar.groovy
+++ vars/scanSonar.groovy
@@ -1,7 +1,7 @@
 def call(String SONAR_URL, String SONAR_AUTH_TOKEN, String WORKSPACE) {
     sh """
         docker run --rm \
-            --network host
+            --network host \
             -e SONAR_HOST_URL="${SONAR_URL}" \
             -e SONAR_TOKEN="${SONAR_AUTH_TOKEN}" \
             -v "${WORKSPACE}:/usr/src" \
