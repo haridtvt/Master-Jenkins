a633a7b583394c159c7325ce0686d90373612d1b
haridtvt <tranvanhaidtvt@gmail.com>
2026-03-25 15:59:38 +0000

    update var build image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,5 @@
 def call(String API_IMAGE, String WORKER_IMAGE, String WEB_IMAGE) {
     sh "docker build -t ${API_IMAGE} api-gateway/ "
-    sh "docker build -t ${WORKER_IMAGE} battle_worker/ "
+    sh "docker build -t ${WORKER_IMAGE} battle-worker/ "
     sh "docker build -t ${WEB_IMAGE} web-portal/ "
 }
