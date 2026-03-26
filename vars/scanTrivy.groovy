390baa924c0b02021195f02d19cdeae1e0dadc6d
haridtvt <tranvanhaidtvt@gmail.com>
2026-03-26 14:11:46 +0000

    Update scanTrivy

--- vars/scanTrivy.groovy
+++ vars/scanTrivy.groovy
@@ -1,4 +1,4 @@
-def call(String API_IMAGE, String WORKER_IMAGE, string WEB_IMAGE) {
+def call(String API_IMAGE, String WORKER_IMAGE, String WEB_IMAGE) {
     sh "trivy image --format template --template '@/home/ec2-user/html.tpl' -o report-image-api.html ${API_IMAGE} || true"
     sh "trivy image --format template --template '@/home/ec2-user/html.tpl' -o report-image-worker.html ${WORKER_IMAGE} || true"
     sh "trivy image --format template --template '@/home/ec2-user/html.tpl' -o report-image-web.html ${WEB_IMAGE} || true"
