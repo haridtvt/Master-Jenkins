d63da4b386aff86494ce2cafee39a8f5cb7d4c6f
haridtvt <tranvanhaidtvt@gmail.com>
2026-03-26 14:20:12 +0000

    Update scanTrivy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -2,5 +2,5 @@ def call(String API_IMAGE, String WORKER_IMAGE, String WEB_IMAGE){
     sh 'docker push ${API_IMAGE}'
     sh 'docker push ${WORKER_IMAGE}'
     sh 'docker push ${WEB_IMAGE}'
-    sh 'docker rmi ${WEB_IMAGE} ${WORKER_IMAGE} ${WEB_IMAGE}'
+    sh 'docker rmi ${WEB_IMAGE} ${WORKER_IMAGE} ${API_IMAGE}'
 }
