e2f86f35f288cb7885cc8c426728cb8cbde9f6d7
haridtvt <tranvanhaidtvt@gmail.com>
2026-03-26 14:10:46 +0000

    Update scanSnyk

--- vars/scanSnyk.groovy
+++ vars/scanSnyk.groovy
@@ -1,4 +1,5 @@
 def call() {
     sh 'snyk code test --json | snyk-to-html -o report-code.html'
     sh 'snyk iac test terraform/ --json | snyk-to-html -o report-terraform.html'
+    sh 'snyk test --all-projects --json | snyk-to-html -o report-dependencies.html'
 }
